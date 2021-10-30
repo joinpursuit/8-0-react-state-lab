283c8130103395eb8c1ad46b03717d33902aeb41
JoseBorbon <joserborbon@gmail.com>
2021-10-29 20:25:22 -0400

    add state to App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { score: 0, currInc: 1, hasWon: false };
+    this.incrementScore = this.incrementScore.bind(this);
+    this.incrementButtonPlus = this.incrementButtonPlus.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  incrementScore() {
+    this.setState(({ score, currInc }) => {
+      return { score: score + currInc };
+    });
+
+    this.setState(({ score }) => {
+      return { hasWon: score >= 100 };
+    });
+  }
+
+  incrementButtonPlus() {
+    if (this.state.score >= 10) {
+      this.setState(({ score, currInc }) => {
+        return {
+          score: score - 10,
+          currInc: currInc + 1,
+        };
+      });
+    } else {
+      alert("You can't afford that!");
+    }
+  }
+
+  reset() {
+    this.setState(() => {
+      return { score: 0, currInc: 1, hasWon: false };
+    });
+  }
+
   render() {
-    return (
+    const result = this.state.hasWon ? (
       <main>
-        <p>React State Lab</p>
+        <h2>You Win!</h2>
+        <button onClick={this.reset}>Play again?</button>
+      </main>
+    ) : (
+      <main>
+        <p>Current Score: {this.state.score}</p>
+        <button onClick={this.incrementScore}>+{this.state.currInc}</button>
+        <button onClick={this.incrementButtonPlus}>
+          Pay 10 points to change from +{this.state.currInc} to +
+          {this.state.currInc + 1}
+        </button>
       </main>
     );
+
+    return <div>{result}</div>;
   }
 }
 
