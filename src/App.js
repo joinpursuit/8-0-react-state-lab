93a12d840711b44202e1ce5cece0adf0989e69c5
BennyZ3 <bennyz3@hotmail.com>
2021-11-04 15:38:40 -0400

    added an actions counter

--- src/App.js
+++ src/App.js
@@ -7,19 +7,15 @@ class App extends React.Component {
     this.state = {
       x: 1,
       currentScore: 0,
+      actions: 0,
     };
   }
   //increase score by current x value and check reaching 100
   xClick = () => {
-    this.setState({ currentScore: this.state.currentScore + this.state.x });
-    if (this.state.currentScore >= 100) {
-      return (
-        <>
-          <h2>You Win!</h2>
-          <button>Play again?</button>
-        </>
-      );
-    }
+    this.setState({
+      currentScore: this.state.currentScore + this.state.x,
+      actions: this.state.actions + 1,
+    });
   };
 
   //attempt to increase x-value if conditions met
@@ -28,24 +24,29 @@ class App extends React.Component {
       this.setState({
         currentScore: this.state.currentScore - 10,
         x: this.state.x + 1,
+        actions: this.state.actions + 1,
       });
     } else {
       alert("You can't afford that!");
     }
   };
 
+  //reset to starting values
   playAgain = () => {
     this.setState({
       currentScore: 0,
       x: 1,
+      actions: 0,
     });
   };
+
   render() {
     return (
       <main>
         {this.state.currentScore < 100 && (
           <div>
             <h1>Current Score: {this.state.currentScore}</h1>
+            <h3>Actions taken: {this.state.actions}</h3>
             <button onClick={this.xClick}>+{this.state.x}</button>
             <p></p>
             <button onClick={this.xIncrease}>
@@ -57,6 +58,7 @@ class App extends React.Component {
         {this.state.currentScore >= 100 && (
           <div>
             <h2>You Win!</h2>
+            <h3>Actions taken: {this.state.actions}</h3>
             <button onClick={this.playAgain}>Play again?</button>
           </div>
         )}
