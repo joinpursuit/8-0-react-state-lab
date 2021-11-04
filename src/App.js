b48d681c8c25d20bc996805c6a1c914da07b6c18
CarlosAMoran <Carlosamoran18@gmail.com>
2021-11-04 17:28:02 -0400

    I think I got it

--- src/App.js
+++ src/App.js
@@ -2,10 +2,37 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      score: 0,
+      interval: 1
+    }
+  }
+  onClicking = () => {
+    this.setState({ score: this.state.score + this.state.interval })
+  }
+  userNotification = () => {
+    alert("You can't afford that!")
+  }
+  currentScore = () => {
+    this.state.interval = this.state.interval + 1
+    this.setState({ score: this.state.score - 10 })
+  }
+  replay = () => {
+    this.state.interval = 1
+    this.setState({ Score: 0 })
+  }
   render() {
+    const { score } = this.state
+    const { interval } = this.state
+    const play = score < 100 ? <>
+      <p>Current Score: {score}</p>
+      <button onClick={this.onClicking}>+{interval}</button>
+      <button onClick={score < 10 ? this.userNotification: this.currentScore}>Pay 10 points to change from +{interval} to +{interval + 1}</button></> : <><p>You Win!</p><button onClick={this.replay}>Play again?</button></>
     return (
       <main>
-        <p>React State Lab</p>
+        {play}
       </main>
     );
   }
