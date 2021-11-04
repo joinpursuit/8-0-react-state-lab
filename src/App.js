4a15dc336f1b0f6260934334d42ec28805c4bab1
Lakisha J <lakishajohnson@pursuit.org>
2021-11-04 19:07:03 -0400

    State Passed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,70 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      add: 1,
+      // result: false,
+    };
+  }
+  //EVENT HANDLER TO INCREASE COUNT, STATE CHANGES TO SET.STATE 
+  handleIncrement = () => {
+    this.setState({
+      //SET.STATE IS THE CURRENT COUNT PLUS THE ADDITIONAL INCREASE
+      count: this.state.count + this.state.add,
+    });
+    // if (this.state.count >= 100) {
+    //   this.setState({
+    //     result: true,
+    //   });
+    // }
+  };
+  //EVENT HANDLER TO PAY 10 POINTS IF COUNT IS GREATER THAN OR EQUAL TO 10, SET IS NOW SET.STATE
+  handleDecrementTen = () => {
+    if (this.state.count >= 10) {
+      this.setState(({ count, add }) => {
+        return { count: count - 10, add: add + 1 };
+      });
+    } else {
+      alert("You can't afford that!");
+    }
+  };
+  resetGame = () => {
+    this.setState({
+      count: 0,
+      add: 1,
+      // result: false,
+    });
+  };
   render() {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+    const { count } = this.state;
+    const { add } = this.state;
+    // const { result } = this.state;
+    
+    // if (!result) {
+    if (count < 100) {
+      // console.log(result)
+      return (
+        <main>
+          <h2>Current Score: {count}</h2>
+          <button onClick={this.handleIncrement}>+{add}</button>
+          <button onClick={this.handleDecrementTen}>
+            Pay 10 points to change from +{add} to +{add + 1}
+          </button>
+        </main>
+        
+      );
+    } else {
+      // console.log(result)
+      return (
+        <main>
+          <h2>You Win!</h2>
+          <button onClick={this.resetGame}>Play again?</button>
+        </main>
+      );
+    }
   }
 }
 
