1568d136d788a9efe026af4f467b510d7fdab863
JoshMarte <joshuelm@gmail.com>
2021-11-04 20:55:30 -0400

    completed lab

--- src/App.js
+++ src/App.js
@@ -2,12 +2,79 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      current: 0,
+      plus: 1,
+      complete: false,
+    };
+  }
+
+  addHandle = () => {
+    if (this.state.current < 100) {
+      this.setState({
+        current: this.state.current + this.state.plus,
+      });
+    } else if (this.state.current >= 100) {
+      this.setState({
+        complete: true,
+      });
+    }
+  };
+
+  bonusHandle = () => {
+    if (this.state.current >= 10) {
+      this.setState({
+        current: this.state.current - 10,
+        plus: this.state.plus + 1,
+      });
+    } else {
+      window.alert("You can't afford that!");
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      current: 0,
+      plus: 1,
+      complete: false,
+    });
+  };
+
   render() {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+    const { current, plus, complete } = this.state;
+
+    if (complete || current >= 100) {
+      return (
+        <main>
+          {/* <h2>Current Score: {current}</h2> */}
+          <br />
+          <br />
+          <br />
+          <div>
+            <h2>You Win!</h2>
+            <button onClick={this.reset}>Play again?</button>
+          </div>
+        </main>
+      );
+    } else {
+      return (
+        <main>
+          <h2>Current Score: {current}</h2>
+          <div>
+            <button id="add" onClick={this.addHandle}>
+              +{plus}
+            </button>
+          </div>
+          <div>
+            <button id="bonus" onClick={this.bonusHandle}>
+              Pay 10 points to change from +{plus} to +{plus + 1}
+            </button>
+          </div>
+        </main>
+      );
+    }
   }
 }
 
