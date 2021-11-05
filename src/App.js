7af745848aeea89300d898579bdf4cf1da6aaac0
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2021-11-05 15:33:28 -0400

    All test passing. add functional components and removed current score

--- src/App.js
+++ src/App.js
@@ -1,14 +1,64 @@
 import React from "react";
 import "./App.css";
-
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+      addScore: 1,
+      firstCount: 10,
+      firstAdd: 1,
+      secondAdd: 2,
+    };
+  }
+  increaseAmount = () => {
+    const { counter, addScore } = this.state;
+    this.setState({
+      counter: counter + addScore,
+    });
+  };
+  pointBoost = () => {
+    const { counter, firstCount, addScore, firstAdd, secondAdd } = this.state;
+    if (counter < firstCount) {
+      alert("You can't afford that!");
+    } else {
+      this.setState({
+        addScore: addScore + 1,
+        counter: counter - firstCount,
+        firstAdd: firstAdd + 1,
+        secondAdd: secondAdd + 1,
+      });
+    }
+  };
+  refresh = () => {
+    this.setState({
+      counter: 0,
+      addScore: 1,
+      firstCount: 10,
+      firstAdd: 1,
+      secondAdd: 1,
+    });
+  };
   render() {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+    const { counter, addScore, firstAdd, secondAdd } = this.state;
+    if (counter >= 100) {
+      return (
+        <main>
+          {/* <div>Current Score: {counter}</div> */}
+          <h2>You Win!</h2>
+          <button onClick={this.refresh}>Play again?</button>
+        </main>
+      );
+    } else {
+      return (
+        <main>
+          <div>Current Score: {counter}</div>
+          <button onClick={this.increaseAmount}>+{addScore}</button>
+          <button onClick={this.pointBoost}>
+            Pay 10 points to change from +{firstAdd} to +{firstAdd+ 1}</button>
+        </main>
+      );
+    }
   }
 }
-
 export default App;
