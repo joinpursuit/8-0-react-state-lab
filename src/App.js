382de868d93eb0d7fef66fe1baeb1eafbc1a9181
rae charles <r_charles87@yahoo.com>
2021-11-04 20:35:55 -0400

    lab finished

--- src/App.js
+++ src/App.js
@@ -2,12 +2,70 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+      pointCount: 0,
+      amount: 1
+    }
+  }
+
+
+totalPoints = () => {
+  const { pointCount, amount } = this.state
+  this.setState({
+    pointCount: pointCount + amount,
+
+  });
+};
+
+increasePoints = () => {
+  const{pointCount, amount} = this.state
+  
+  if (pointCount >= 10){
+    
+    this.setState({
+      amount: (amount+1),
+      pointCount: pointCount - 10,
+    })
+  }else{
+   alert("You can't afford that!")
+  }
+}
+
+reset = () =>{
+  this.setState({
+    pointCount: 0,
+    amount: 1
+  })
+}
+
   render() {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+
+  const { pointCount, amount } = this.state;
+
+    if (pointCount < 100){
+      return (
+          <main>
+            <h1>Current Score: {pointCount}</h1>
+            <div className="addButton">
+              <button onClick={this.totalPoints}>+{amount}</button>
+            </div>
+            <div className="payButton">
+              <button onClick={this.increasePoints}>Pay 10 points to change from +{amount} to +{amount + 1}</button>
+            </div>
+          </main>
+        )
+      } else {
+      return(
+        <main>
+          <h2>You Win!</h2>
+          <button onClick={this.reset}>Play again?</button>
+        </main>
+      )
+
+    }
   }
 }
 
