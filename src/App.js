f93bff4c839758d2c9dfe135664edcd55da4e6d6
Theodore <theo.frazier@icloud.com>
2021-11-05 11:45:56 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -9,43 +9,68 @@ class App extends React.Component {
 
 
     this.state = {
-      totalNum: 0
+      totalNum: 0,
+      incrementor: 1
     }
   }
 
-  handleClick = () => {
-    this.totalNum += 1
 
+  handleClick = () => {
+    const { totalNum, incrementor } = this.state
 
     this.setState({
-      totalNum: this.state.totalNum + 1
+      totalNum: totalNum + incrementor
     })
 
   }
 
   coinHandle = () => {
 
-    if (this.state.totalNum < 10){
-      window.alert("You can't afford that!")
+    const { totalNum, incrementor } = this.state
+
+    if (totalNum < 10) {
+      alert("You can't afford that!")
     } else {
       this.setState({
-        totalNum: this.state.totalNum -10
+        totalNum: totalNum - 10,
+        incrementor: incrementor + 1
       })
     }
-  
-}
+
+  }
+
+  playAgain = () => {
+    this.setState ({
+      totalNum: 0,
+      incrementor: 1
+    })
+  }
 
 
 
-render() {
-  return (
-    <main>
-      <h1>Your Current Score: {this.state.totalNum}</h1>
-      <button onClick={this.handleClick}>Add 1</button>
-      <button onClick = {this.coinHandle}>Pay 10 points to change from</button>
-    </main>
-  );
+  render() {
+    const { totalNum, incrementor } = this.state
+    if (totalNum < 100) {
+      return (
+        <main>
+          <h1>Current Score: {totalNum}</h1>
+          <button onClick={this.handleClick}>+{incrementor}</button>
+          <button onClick={this.coinHandle}>Pay 10 points to change from +{incrementor} to +{incrementor + 1} </button>
+        </main>
+      )
+    } else {
+      return (
+        <main>
+          <h2>You Win!</h2>
+          <button onClick={this.playAgain}>Play again?</button>
+        </main>
+      )
+    }
+  }
+
+
 }
-}
+
+
 
 export default App;
