db77e38cb3d8cdb5d319d021cf02d4c96a859115
Mukayila ALAO <mukayilaalao@pursuit.org>
2021-11-05 10:34:52 -0400

    Working game

--- src/App.js
+++ src/App.js
@@ -2,12 +2,58 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state={
+      score:0,
+      increment:1
+    }
+  }
+  handleIncrementScore=()=>{
+    this.setState({
+      score:this.state.score+this.state.increment
+    })
+  }
+  handlePay10=()=>{
+    if(this.state.score<10) alert("You can't afford that!");
+    else {
+      this.setState({
+        score:this.state.score-10,
+        increment:this.state.increment +1
+      })
+    }
+  }
+  handleReset=()=>{
+    this.setState({
+      score:0,
+      increment:1
+    })
+  }
   render() {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+    if(this.state.score<100) {
+      return (
+        <main>
+          
+            <h1>Current Score: {this.state.score}</h1>
+            <button onClick={this.handleIncrementScore} style={{width:"50px"}}>+{this.state.increment}</button>
+            <button onClick={this.handlePay10} style={{width:"250px"}}>Pay 10 points to change from +{this.state.increment} to +{this.state.increment+1}</button>
+          
+        </main>
+      );
+
+    } else {
+      return(
+        <main>
+          
+            {/* <h1>Current Score: {this.state.score}</h1> */}
+            <h2>You Win!</h2>
+            <button onClick={this.handleReset} style={{width:"100px"}}>Play again?</button>
+         
+        </main>
+
+      )
+    }
+    
   }
 }
 
