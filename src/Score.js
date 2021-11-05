c882fa3d31c990b3b1334d6046d6abdf5a115de1
Deborah Campos <631care@gmail.com>
2021-11-05 11:26:46 -0400

    functional page

--- src/Score.js
+++ src/Score.js
@@ -25,17 +25,32 @@ increaseAddPoints =()=>{
         alert("You can't afford that!")
     }
 }
+playAgain =()=>{
+    this.setState({
+        count: 0,
+        addPoints: 1
+    })
+}
 
 
     render(){
-
-        return (
-            <div id="score">
-                <h1 className = "current-score">Current Score: {this.state.count} </h1>
-                <button onClick={this.addOne}> + {this.state.addPoints} </button>
-                <button onClick={this.increaseAddPoints}>Pay 10 points to change from +{this.state.addPoints} to +{this.state.addPoints + 1}</button>
-            </div>
-        )
+        if (this.state.count < 100){
+            return (
+                <div id="score">
+                    <h1 className = "current-score">Current Score: {this.state.count} </h1>
+                    <button onClick={this.addOne}> + {this.state.addPoints} </button>
+                    <button onClick={this.increaseAddPoints}>Pay 10 points to change from +{this.state.addPoints} to +{this.state.addPoints + 1}</button>
+                </div>
+            )
+        } else if (this.state.count >= 100){
+            return (
+                <div>
+                    <h2> You Win!</h2>
+                    <button onClick={this.playAgain}>Play again?</button>
+                </div>
+                
+            )     
+        }
     }
 }
 
