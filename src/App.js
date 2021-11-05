2eb1f90ac0396933326ff83e42766b557bd22b42
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-05 00:01:43 -0400

    added code with buttons and methods

--- src/App.js
+++ src/App.js
@@ -2,10 +2,43 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentScore: 0,
+      points: 1,
+    }
+  }
+
+  increaseScore = () => {
+    const { currentScore, points } = this.state;
+
+    if(currentScore >= 10){
+      this.setState({
+        currentScore: currentScore - 10,
+        points: points + 1,
+      })
+    } else {
+      alert("You can't afford that!")
+    }
+  }
+
+  sumOfPoints = () => {
+    const { currentScore, points } = this.state;
+    this.setState({
+      currentScore: currentScore + points,
+    })
+  }
+
   render() {
+    const { currentScore, points } = this.state;
+
     return (
       <main>
-        <p>React State Lab</p>
+        <h1 className="title">Current Score:<span className="currentScore">{ currentScore }</span></h1>
+        <button onClick={ this.sumOfPoints }>+{points}</button>
+        <button onClick={ this.increaseScore }>Pay 10 points to change from +{points} to +{points + 1}</button>
       </main>
     );
   }
