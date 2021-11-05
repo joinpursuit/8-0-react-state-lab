315925303705faa99248450c57604fbf67977398
Name Ping Louie <namepinglouie@gmail.com>
2021-11-05 14:48:12 -0400

    worked with Charlie and passed all tests

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
 import React from "react";
 import "./App.css";
 
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+        score: 0,
+        increment: 1
+    }
+  }
+
+  incrementMethod = () => {
+    const {score, increment} = this.state; 
+    this.setState({
+        score: score + increment
+    })
+  }
+
+  scoreUpdateMethod = () => {
+    this.state.increment = this.state.increment + 1
+    this.setState({
+        score: this.state.score - 10
+    })
+  }
+
+  promptMethod = () => {
+    alert("You can't afford that!");
+  }
+
+  playAgain = () => {
+    this.setState({
+        score: 0,
+        increment: 1
+    })
+  }
+  
   render() {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+    const {score, increment} = this.state;
+    const nextIncrement = increment + 1;
+
+    if(score < 100) {
+      return (
+          <main className="display">
+              <h1>Current Score: {score}</h1>
+              <button onClick={this.incrementMethod}>+{increment}</button>
+              <br />
+              <button onClick={score < 10 ? this.promptMethod : this.scoreUpdateMethod}>Pay 10 points to change from +{increment} to +{nextIncrement}</button>
+          </main>
+      )
+    } else {
+      return (
+          <main className="resetGame">
+              <h2>You Win!</h2>
+              <button onClick={this.playAgain}>Play again?</button>
+          </main>
+      )
+    }
   }
 }
 
