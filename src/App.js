af35ad36299e2db22483ab16eaa3617a00be465b
Billy Dominique <billydominique@pursuit.org>
2021-11-05 09:47:52 -0400

    Passed both Test

--- src/App.js
+++ src/App.js
@@ -2,12 +2,55 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      increment: 1
+    };
+  }
+
+  //event handler to increase counter
+  countClick = () => {
+    this.setState({ count: this.state.count + this.state.increment });
+  };
+
+  alertMessage = () => {
+    alert("You can't afford that!")
+  }
+
+  //event handler to increase increment
+  incrementClick = () => {
+    this.setState({ count: this.state.count - 10 });
+    this.setState({ increment: this.state.increment + 1 });
+  }
+
   render() {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+    const { count } = this.state;
+    const { increment } = this.state;
+    // const number = 1 + Number({ increment: increment })
+    // console.log(number)
+
+
+    if (count < 100) {
+      return (
+        <main>
+          <p> Current Score: {count} </p>
+          <button onClick={this.countClick}> +{increment}</button>
+          <button onClick=
+            {count < 10 ? this.alertMessage : this.incrementClick}
+          >  Pay 10 points to change from +{increment} to +{increment + 1}
+          </button>
+        </main >
+      );
+    } else {
+      return (
+        <main>
+          <h1>You Win!</h1>
+          <button onClick={() => this.setState({ count: 0, increment: 1 })}> Play again? </button>
+        </main>
+      )
+    }
   }
 }
 
