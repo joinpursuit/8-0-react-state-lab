443d47d535d29172b9cc395eb5b3041ff2d2db36
Theodore <theo.frazier@icloud.com>
2021-11-05 10:35:36 -0400

    got coinHandle to work

--- src/App.js
+++ src/App.js
@@ -23,15 +23,29 @@ class App extends React.Component {
 
   }
 
+  coinHandle = () => {
 
-  render() {
-    return (
-      <main>
+    if (this.state.totalNum < 10){
+      window.alert("You can't afford that!")
+    } else {
+      this.setState({
+        totalNum: this.state.totalNum -10
+      })
+    }
+  
+}
+
+
+
+render() {
+  return (
+    <main>
       <h1>Your Current Score: {this.state.totalNum}</h1>
-        <button onClick = {this.handleClick}>Add 1</button>
-      </main>
-    );
-  }
+      <button onClick={this.handleClick}>Add 1</button>
+      <button onClick = {this.coinHandle}>Pay 10 points to change from</button>
+    </main>
+  );
+}
 }
 
 export default App;
