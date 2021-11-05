84d77eb21565972abd16a73d8b0aa6b833bf6a95
Jenna Qian <jiayin1996@gmail.com>
2021-11-04 23:30:13 -0400

    Passed half of cypress tests

--- src/App.js
+++ src/App.js
@@ -2,10 +2,54 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      count: 0,
+      one: 1,
+    }
+  }
+
+  addOne = () => {
+    const { count, one } = this.state
+
+    this.setState({
+      count: count + 1
+    })
+  }
+
+  increaseCount = () => {
+    const { count, one } = this.state
+
+    if(count >= 10) {
+      this.setState ({
+        count: count - 10,
+        one: one + 1,
+      })
+    } else {
+      window.alert("You can't afford that!")
+    }
+  }
+
+  // resetCount = () => {
+  //   this.setState ({
+  //     count: 0,
+  //   })
+  // }
+  
   render() {
+    const { count, one } = this.state
+
     return (
       <main>
-        <p>React State Lab</p>
+        <h2>Current Score: {count}</h2>
+          <div>
+            <button type="submit" onClick={this.addOne}> +{one} </button>
+          </div>
+          <div>
+            <button type="submit" onClick={this.increaseCount}>Pay 10 points to change from +{one} to +{one + 1}</button>
+          </div>
       </main>
     );
   }
