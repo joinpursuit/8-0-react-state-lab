beaa3dc4a05993de72b73e0eb2d08862982b8c5c
Angie G <angieg@Angies-MBP.fios-router.home>
2021-11-05 10:00:28 -0400

    Lab

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "./App.css";
+import Increment from "./Increment"
+import Score from "./PayPoints"
+import playAgain from ".playAgain"
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentScore: 0,
+    }
+  }
+
+  // event handler 
+  changeScore = () => {
+    this.setState({ currentScore: this.state.currentScore + 1})
+  };
+
   render() {
     return (
       <main>
-        <p>React State Lab</p>
+        <p>Current Score: {currentScore}</p>
       </main>
+
+
     );
   }
 }
---
+++ src/Increment.js
@@ -0,0 +1,8 @@
+const Increment = (props) => {
+    return (
+        <button onClick={props.changeScore}>Click
+        </button>
+    );
+};
+
+export default Increment;
