85384bbf3353cf5c38b40088ec2abe43ebc008b1
Laiba Sajid <sajid.laiba378@gmail.com>
2021-11-05 00:38:44 -0400

    first attempt

--- src/App.js
+++ src/App.js
@@ -2,12 +2,61 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      count:0,
+      pointNum: 1,
+    }
+  }
+
+  increaseCount = () => {
+    const {count, pointNum} = this.state;
+    if(count >= 10){
+      this.setState({
+        pointNum: pointNum + 1,
+        count: count - 10,
+      })
+    }else{
+      alert("You can't afford that!");
+    }
+  }
+
+  pointUp = () => {
+    const {count, pointNum} = this.state;
+    this.setState({
+      count: count + pointNum
+    })
+  }
+
+  handleReset = () => {
+    const {count, pointNum} = this.state;
+    this.setState({
+      count:0,
+      pointNum: 1,
+    })
+  }
+
   render() {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+    const {count, pointNum} = this.state;
+    if(count < 100){
+      return (
+        <main>
+          <h1 className='title'> Current Score: <span className='currentScore'> {count}</span></h1>
+          <button onClick={this.pointUp}> +{pointNum} </button>
+          <button onClick={this.increaseCount}>Pay 10 points to change from +{pointNum} to +{pointNum+1}</button>
+        </main>
+      );
+    }else {
+      return(
+        <main>
+          <h2>You Win!</h2>
+          <button onClick={this.handleReset}>Play again?</button>
+        </main>
+
+      )
+    }
   }
 }
 
