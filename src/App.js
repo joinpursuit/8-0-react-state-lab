5f480605bb30a6614697e07e49485703e050cc8e
caveman49 <caveman4949@gmail.com>
2021-11-05 09:52:29 -0400

    created_clicker_using_react_props_and_state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,55 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      increCounter: 1,
+    };
+  }
+
+  increButton = () => {
+    this.setState({
+      count: this.state.count + this.state.increCounter,
+    });
+  };
+
+  decreButton = () => {
+    if (this.state.count < 10) {
+      alert("You can't afford that!");
+      return;
+    }
+    this.setState({
+      count: this.state.count - 10,
+      increCounter: this.state.increCounter + 1,
+    });
+  };
+
+  win = () => {
+    this.setState({
+      count: 0,
+      increCounter: 1,
+    });
+  };
+
   render() {
-    return (
+    const { count, increCounter } = this.state;
+
+    return this.state.count >= 100 ? (
+      <div>
+        <h2>You Win!</h2>
+        <button onClick={this.win}>Play again?</button>
+      </div>
+    ) : (
       <main>
-        <p>React State Lab</p>
+        <h1>Current Score: {count}</h1>
+
+        <button onClick={this.increButton}>+{increCounter}</button>
+        <button onClick={this.decreButton}>
+          Pay 10 points to change from +{increCounter} to +{increCounter + 1}
+        </button>
+        <p>Clicked {count} times</p>
       </main>
     );
   }
