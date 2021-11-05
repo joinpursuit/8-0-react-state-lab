2f2781e95e4dbe609e23956105e9eb062959a39d
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-05 00:10:17 -0400

    passed all tests

--- src/App.js
+++ src/App.js
@@ -31,16 +31,32 @@ class App extends React.Component {
     })
   }
 
+  reset = () => {
+    this.setState({
+      currentScore: 0,
+      points: 1,
+    })
+  }
+
   render() {
     const { currentScore, points } = this.state;
 
-    return (
-      <main>
-        <h1 className="title">Current Score:<span className="currentScore">{ currentScore }</span></h1>
-        <button onClick={ this.sumOfPoints }>+{points}</button>
-        <button onClick={ this.increaseScore }>Pay 10 points to change from +{points} to +{points + 1}</button>
-      </main>
-    );
+    if (currentScore < 100){
+      return (
+        <main>
+          <h1 className="title">Current Score: <span className="currentScore">{ currentScore }</span></h1>
+          <button onClick={ this.sumOfPoints }>+{points}</button>
+          <button onClick={ this.increaseScore }>Pay 10 points to change from +{points} to +{points + 1}</button>
+        </main>
+      )
+    } else {
+      return (
+        <main>
+          <h2>You Win!</h2>
+          <button onClick={ this.reset }>Play again?</button>
+        </main>
+      )
+    } 
   }
 }
 
