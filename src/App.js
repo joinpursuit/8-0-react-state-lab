ef50c83c5211c1ebe9c6e72a2f216fa44b39e7cb
andres ballares <andr3sb@hotmail.com>
2021-11-04 22:11:37 -0400

    lab is functional

--- src/App.js
+++ src/App.js
@@ -2,12 +2,60 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor () {
+    super ()
+    this.state = {
+      score: 0,
+      currentValue: 1,
+      nextValue: 2,
+    }
+  }
+
+  increaseScore = () => {
+    this.setState({ score: this.state.score + this.state.currentValue });
+  }
+
+  message = () => {
+    if (this.state.score < 10){
+        alert("You can't afford that!");
+    } else {
+    this.setState({
+      score: this.state.score - 10,
+      currentValue: this.state.currentValue + 1,
+      nextValue: this.state.nextValue + 1,
+      });
+    }
+  }
+
+reset = () => {
+    this.setState({
+      score: this.state.score - this.state.score,
+      currentValue: this.state.currentValue - this.state.currentValue + 1,
+      nextValue: this.state.nextValue - this.state.nextValue + 2,
+    })
+  }
+
+
   render() {
+    if (this.state.score < 100) {
     return (
       <main>
-        <p>React State Lab</p>
+        
+        <h1>Current Score: {this.state.score}</h1>
+        <button onClick={this.increaseScore}> +{this.state.currentValue} </button>
+        <button onClick={this.message}>Pay 10 points to change from +{this.state.currentValue} to 
+        +{this.state.nextValue} </button>
+        
       </main>
     );
+    } else {
+      return (
+        <>
+          <h2>You Win!</h2>
+          <button onClick={this.reset}>Play Again?</button>
+        </>
+      )
+    } 
   }
 }
 
