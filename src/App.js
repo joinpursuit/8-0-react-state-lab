99edc81dd8035ad26db2ea7dbe9d66e6362b608a
Theodore <theo.frazier@icloud.com>
2021-11-04 23:33:41 -0400

    counter working

--- src/App.js
+++ src/App.js
@@ -2,10 +2,33 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+
+  constructor() {
+
+    super()
+
+
+    this.state = {
+      totalNum: 0
+    }
+  }
+
+  handleClick = () => {
+    this.totalNum += 1
+
+
+    this.setState({
+      totalNum: this.state.totalNum + 1
+    })
+
+  }
+
+
   render() {
     return (
       <main>
-        <p>React State Lab</p>
+      <h1>Your Current Score: {this.state.totalNum}</h1>
+        <button onClick = {this.handleClick}>Add 1</button>
       </main>
     );
   }
