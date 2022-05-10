eeabcf726cd0528b0d38e40bbcdf7dd20e83ced5
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2022-05-10 19:12:58 -0400

    stateLab

--- src/App.js
+++ src/App.js
@@ -2,12 +2,61 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentScore: 0,
+      addedScore: 1
+    }
+  }
+  increaseScore = () => {
+    this.setState({
+      currentScore: this.state.currentScore + this.state.addedScore,
+    })
+  };
+
+  increaseScore2 = () => {
+    if (this.state.currentScore >= 10) {
+      this.setState({
+        currentScore: this.state.currentScore - 10,
+        addedScore: this.state.addedScore + 1
+      })
+    } else {
+      alert(`You can't afford that!`);
+    }
+  }
+
+  restart = () => {
+    this.setState({
+      currentScore: 0,
+      addedScore: 1
+    })
+  }
+
   render() {
+    const { currentScore, addedScore } = this.state;
+    if (currentScore < 100) {
     return (
       <main>
         <p>React State Lab</p>
-      </main>
-    );
+          
+            <p>Current Score: {currentScore}</p>
+            <button onClick={this.increaseScore}>+{addedScore}</button>
+            <br />
+            <button onClick={this.increaseScore2}>Pay 10 points to change from +{addedScore} to +{addedScore + 1}</button>
+          </main>
+        )
+      } else {
+        return (
+        
+          <main>
+            <h2>You Win!</h2>
+            <button onClick={this.restart}>Play again?</button>
+          </main>
+        )
+      }
+    
+  
   }
 }
 
