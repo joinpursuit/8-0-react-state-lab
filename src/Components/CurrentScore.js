5248c3fe1d3970f8f1bff99656e97f0a3d16c5a7
lily08 <liliangelxd@gmail.com>
2022-05-11 15:47:32 -0400

    fix typo

--- src/Components/CurrentScore.js
+++ src/Components/CurrentScore.js
@@ -45,7 +45,7 @@ class CurrentScore extends React.Component {
     } else {
       return (
         <div>
-          <h2>CurrentScore: {this.state.count}!</h2>
+          <h2>Current Score: {this.state.count}</h2>
           <h2>You Win!</h2>
           <button onClick={this.playbutton}>Play again?</button>
         </div>
@@ -56,7 +56,4 @@ class CurrentScore extends React.Component {
 
 export default CurrentScore
 
-//pay 10 deduct 10 increase by 2
-//hint count change and the increment change
-// when pay 10 increase the +1 to +2 and +2 to +3 ?
-// if -10 add 1 and keep increasing and chaning the button on the current score?
+
