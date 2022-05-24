165910e070050d3eba1095a9c22f02a73889202e
Victor E Lopez <velbx51594@macbook-air.myfiosgateway.com>
2022-05-23 22:54:32 -0400

    react state lab

--- src/App.js
+++ src/App.js
@@ -1,13 +1,73 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 class App extends React.Component {
-  render() {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      incrementor: 1,
+    };
+  }
+
+  increaseScore = () => {
+    this.setState({
+      count: this.state.count + this.state.incrementor,
+    });
+  };
+
+  payTenPoints = () => {
+    if (this.state.count < 10) {
+      alert("You can't afford that!");
+    } else {
+      this.setState({
+        count: this.state.count - 10,
+        increase: ++this.state.incrementor,
+      });
+    }
+  };
+
+  resetGame = () => {
+    this.setState({
+      count: 0,
+      incrementor: 1,
+    });
+  };
+
+  render () {
+  const { count, incrementor } = this.state;
+    if (count < 100) {
+      return (
+        <main>
+          {count < 100 ? (
+            <h1>Current Score: {count} points</h1>
+          ) : (
+            <h1>You Score: {count} points</h1>
+          )}
+
+          <button onClick={this.increaseScore} className='incrementor'>
+            +{incrementor}
+          </button>
+          <button onClick={this.payTenPoints} className='pay-ten'>
+            Pay 10 points to change from +{incrementor} to +{incrementor + 1}
+          </button>
+        </main>
+      );
+    } else {
+      return (
+        <main>
+          {count >= 100 ? (
+            <h1>You Score: {count} points</h1>
+          ) : (
+            <h1>Current Score: {count} points</h1>
+          )}
+          <h2>You Win!</h2>
+          <button onClick={this.resetGame} className='play-again'>
+            Play again?
+          </button>
+        </main>
+      );
+    }
   }
 }
 
