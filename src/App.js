5e71163fee9073d74c622a7f0a3648c145559dcc
Teraesa  Metivier <teraesam@yahoo.com>
2022-06-03 11:36:05 -0400

    lab complete

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import ClickerGame from "./ClickerGame";
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <main>
-        <ClickerGame />
-      </main>
-    );
-  }
-}
+const App = () => {
+  return (
+    <main>
+      <ClickerGame />
+    </main>
+  );
+};
 
 export default App;
