e4edaa54be92c646086121ee83d910d832fa830f
Jamal Ruiz <jamalruiz@jamals-air-2.mynetworksettings.com>
2022-06-05 12:40:53 -0400

    LAB COMPLETE

--- test.js
+++ test.js
@@ -1,56 +1,124 @@
-import React, {useState} from "react";
-import "./App.css";
+// import React, {useState} from "react";
+// import "./App.css";
 
-const App = () => {
-   const [state,setState]= useState({ count: 0,
-    incrementor: 1})  
-    const {count, incrementor} = state
+// const App = () => {
+//    const [state,setState]= useState({ count: 0,
+//     incrementor: 1})  
+//     const {count, incrementor} = state
 
-  const handleClick = () => {
-  setState(count + incrementor)
-  }
+//   const handleClick = () => {
+//   setState(count + incrementor)
+//   }
   
   
 
-  const handleIncrementor = () => {
-    if (count < 10) {
-      alert(`You can't afford that!`);
-    } else {
-      setState(count - 10,incrementor + 1)
-      };
-    }
-    // const { count } = this.state;
-    // if (this.state.count > 100) {
-    //   return (
-    //     <main>
-    //       <h2>You Win!</h2>
-    //       <button onClick={() => window.location.reload()}>Play again?</button>
-    //     </main>
-    //   );
-    // } else {
-      return (
-        <main>
-          <h1>Current Score: {count}</h1>
-          <button onClick={handleClick}>+ </button>
-          <p></p>
-          {/* <button onClick={handleIncrementor}>
-            Pay 10 points to change from +{incrementor} to +
-            {this.state.incrementor + 1}
-          </button> */}
-        </main>
-      );
+//   const handleIncrementor = () => {
+//     if (count < 10) {
+//       alert(`You can't afford that!`);
+//     } else {
+//       setState(count - 10,incrementor + 1)
+//       };
+//     }
+//     // const { count } = this.state;
+//     // if (this.state.count > 100) {
+//     //   return (
+//     //     <main>
+//     //       <h2>You Win!</h2>
+//     //       <button onClick={() => window.location.reload()}>Play again?</button>
+//     //     </main>
+//     //   );
+//     // } else {
+//       return (
+//         <main>
+//           <h1>Current Score: {count}</h1>
+//           <button onClick={handleClick}>+ </button>
+//           <p></p>
+//           {/* <button onClick={handleIncrementor}>
+//             Pay 10 points to change from +{incrementor} to +
+//             {this.state.incrementor + 1}
+//           </button> */}
+//         </main>
+//       );
       
-    }
+//     }
    
   
   
 
-  // playagain =() =>{
-  //   this.setState({
-  //     count:0,
-  //     incrementor: 1
-  //   })
-  // } OPTIONAL ROUTE
+//   // playagain =() =>{
+//   //   this.setState({
+//   //     count:0,
+//   //     incrementor: 1
+//   //   })
+//   // } OPTIONAL ROUTE
+
+// //   render() {
+// //     const { count } = this.state;
+// //     if (this.state.count > 100) {
+// //       return (
+// //         <main>
+// //           <h2>You Win!</h2>
+// //           <button onClick={() => window.location.reload()}>Play again?</button>
+// //         </main>
+// //       );
+// //     } else {
+// //       return (
+// //         <main>
+// //           <h1>Current Score: {count}</h1>
+// //           <button onClick={handleClick}>+ {this.state.incrementor}</button>
+// //           <p></p>
+// //           <button onClick={this.handleIncrementor}>
+// //             Pay 10 points to change from +{this.state.incrementor} to +
+// //             {this.state.incrementor + 1}
+// //           </button>
+// //         </main>
+// //       );
+      
+// //     }
+   
+// //   }
+// // }
+
+// export default App;
+
+
+
+// CLASSSS COMPONENTS WAY ////////
+// import React from "react";
+// import "./App.css";
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       count: 0,
+//       incrementor: 1,
+
+//     };
+//   }
+
+//   handleClick = () => {
+//     this.setState({
+//       count: this.state.count + this.state.incrementor,
+//     });
+//   };
+
+//   handleIncrementor = () => {
+//     if (this.state.count < 10) {
+//       alert(`You can't afford that!`);
+//     } else {
+//       this.setState({
+//         count: this.state.count - 10,
+//         incrementor: this.state.incrementor + 1,
+//       });
+//     }
+//   };
+//   // playagain =() =>{
+//   //   this.setState({
+//   //     count:0,
+//   //     incrementor: 1
+//   //   })
+//   // } OPTIONAL ROUTE
 
 //   render() {
 //     const { count } = this.state;
@@ -65,7 +133,7 @@ const App = () => {
 //       return (
 //         <main>
 //           <h1>Current Score: {count}</h1>
-//           <button onClick={handleClick}>+ {this.state.incrementor}</button>
+//           <button onClick={this.handleClick}>+ {this.state.incrementor}</button>
 //           <p></p>
 //           <button onClick={this.handleIncrementor}>
 //             Pay 10 points to change from +{this.state.incrementor} to +
@@ -79,5 +147,4 @@ const App = () => {
 //   }
 // }
 
-export default App;
-
+// export default App;
