e08b93c4abbd40b607aee12dde5b144f9794faad
luke kinoshi <lukekinoshi@lukes-MacBook-Air.local>
2022-06-12 15:41:18 -0400

    addluke

--- src/App.js
+++ src/App.js
@@ -2,12 +2,64 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      increment: 1,
+    };
+  }
+
+  handleClick = () => {
+
+    this.setState({
+      count: (this.state.count + this.state.increment)
+    });
+  };
+
+  subtract = () => {
+    if (this.state.count < 10) {
+      window.alert("You can't afford that!");
+    } else {
+      this.setState({
+        count: this.state.count - 10,
+        increment: this.state.increment + 1,
+      });
+    }
+  };
+
+  restart = () => {
+    this.setState({
+      count: 0,
+      increment: 1,
+    });
+  };
+
   render() {
     return (
       <main>
         <p>React State Lab</p>
       </main>
     );
+    if (this.state.count < 100) {
+      return (
+        <div>
+          <h1>Current Score: {this.state.count}</h1>
+          <button onClick={this.handleClick}>+{this.state.increment}</button>
+          <button onClick={this.subtract}>
+            Pay 10 points to change from +{this.state.increment} to +{this.state.increment + 1}
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>Winning Score: {this.state.count}</h1>
+          <h2>You Win!</h2>
+          <button onClick={this.restart}>Play again?</button>
+        </div>
+      );
+    }
   }
 }
 
