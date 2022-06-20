fa05fd32308f331d4edfdfae7956b7b540075190
KamNotCam <Kedari.a.matthews@gmail.com>
2022-06-20 07:41:14 -0400

    Final changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
-class App extends React.Component {
-  render() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [increment, setIncrement] = useState(1);
+
+  const incrementCount = () => {
+    setCount(count + increment);
+  };
+
+  const pay10 = () => {
+    if (count < 10) {
+      alert("You can't afford that!");
+    } else {
+      setCount(count - 10);
+      setIncrement(increment + 1);
+    }
+  };
+
+  const playagain = () => {
+    setCount(0);
+    setIncrement(1);
+  };
+
+  if (count < 100) {
     return (
       <main>
-        <p>React State Lab</p>
+        <p>Current Score: {count}</p>
+        <button onClick={incrementCount}>+{increment}</button>
+        <br></br>
+        <button onClick={pay10}>
+          Pay 10 points to change from +{increment} to +{increment + 1}
+        </button>
+      </main>
+    );
+  } else {
+    return (
+      <main>
+        <h2>You Win!</h2>
+        <button onClick={playagain}>Play again?</button>
       </main>
     );
   }
-}
+};
 
 export default App;
