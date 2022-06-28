696221f92b6469980504d233915846a4240d6738
Tom LaTulipe <tomlatulipe@pursuit.org>
2022-06-27 22:00:45 -0400

    lookin' good

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
-  }
-}
+const App = () => {
+  const [score, setScore] = useState(0);
+  const [scorePlus, setPlus] = useState(1);
+  const [more, evenMore] = useState(2);
+
+  const numberGoesUp = () => {
+    return setScore((prev) => {
+      return (prev += scorePlus);
+    });
+  };
+
+  const morePower = () => {
+    if (score < 10) {
+      alert("You can't afford that!");
+      return;
+    }
+    setScore((prevScore) => {
+      return prevScore - 10;
+    });
+    evenMore((prevMore) => {
+      return (prevMore += 1);
+    });
+    setPlus((prevPlus) => {
+      return (prevPlus = more);
+    });
+  };
+
+  return (
+    <main>
+      <h1>Current Score: {score}</h1>
+      <button onClick={numberGoesUp}>+{scorePlus}</button>
+      <br></br>
+      <button onClick={morePower}>
+        Pay 10 points to change from +{scorePlus} to +{more}
+      </button>
+    </main>
+  );
+};
 
 export default App;
