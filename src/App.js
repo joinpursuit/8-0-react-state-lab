478720fd3363bf0b95dafbbe30c24ab5e67ff4e2
Tom LaTulipe <tomlatulipe@pursuit.org>
2022-06-27 23:58:13 -0400

    passing all

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
   const [score, setScore] = useState(0);
   const [scorePlus, setPlus] = useState(1);
   const [more, evenMore] = useState(2);
+  const [win, setWin] = useState(false);
+  const [visible, setVisible] = useState(false);
+
+  const restartGame = () => {
+    setVisible();
+    setScore(0);
+    setPlus(1);
+    evenMore(2);
+    setWin(false);
+  };
 
   const numberGoesUp = () => {
     return setScore((prev) => {
@@ -29,14 +39,31 @@ const App = () => {
     });
   };
 
+  useEffect(() => {
+    if (score >= 100) {
+      setVisible(true);
+    }
+    return () => {};
+  }, [score]);
+
   return (
     <main>
-      <h1>Current Score: {score}</h1>
-      <button onClick={numberGoesUp}>+{scorePlus}</button>
-      <br></br>
-      <button onClick={morePower}>
-        Pay 10 points to change from +{scorePlus} to +{more}
-      </button>
+      {visible ? (
+        <>
+          <h2>You Win!</h2>
+          <button onClick={restartGame}>Play again?</button>
+        </>
+      ) : null}
+      {!visible ? (
+        <>
+          <h1>Current Score: {score}</h1>
+          <button onClick={numberGoesUp}>+{scorePlus}</button>
+          <br></br>
+          <button onClick={morePower}>
+            Pay 10 points to change from +{scorePlus} to +{more}
+          </button>
+        </>
+      ) : null}
     </main>
   );
 };
