b6ded359ed5302ce47980e27ab79bf149a11d7ef
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-10-07 16:49:39 -0400

    good progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
-function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+function App() {
+  const [points, setPoints] = useState(0);
+  const [incrementor, setIncrementor] = useState(1);
+  // const [visibility, setVisibility] = useState(`hidden`);
+
+  const increasePoints = () => {
+    setPoints(points + incrementor);
+    // if (points > 99) {
+    // }
+  };
+
+  const updateIncrementor = () => {
+    if (points > 9) {
+      setIncrementor(incrementor + 1);
+      setPoints(points - 10);
+    } else {
+      alert(`You can't afford that!`);
+    }
+  };
+
+  return (
+    <main>
+      <h1>Current Score: {points}</h1>
+      <button onClick={increasePoints}>+{incrementor}</button>
+      <br />
+      <button onClick={updateIncrementor}>
+        Pay 10 points to change from +{incrementor} to +{incrementor + 1}
+      </button>
+    </main>
+  );
 }
 
 export default App;
