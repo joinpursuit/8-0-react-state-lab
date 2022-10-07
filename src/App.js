98f58d1b283d3e4c57819e73c04424a6e4ee6cb8
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-10-07 18:00:44 -0400

    all functionality, need to change code for 1 test

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import "./App.css";
 function App() {
   const [points, setPoints] = useState(0);
   const [incrementor, setIncrementor] = useState(1);
-  // const [visibility, setVisibility] = useState(`hidden`);
+  const [visibility, setVisibility] = useState(`hidden`);
+
+  const changeVisibility = () => {
+    setVisibility(`notHidden`);
+  };
 
   const increasePoints = () => {
-    setPoints(points + incrementor);
-    // if (points > 99) {
-    // }
+    const newValue = points + incrementor;
+    setPoints(newValue);
+    if (newValue > 99) {
+      changeVisibility();
+    }
   };
 
   const updateIncrementor = () => {
@@ -22,14 +28,26 @@ function App() {
     }
   };
 
+  const resetGame = () => {
+    setPoints(0);
+    setIncrementor(1);
+    setVisibility(`hidden`);
+  };
+
   return (
     <main>
-      <h1>Current Score: {points}</h1>
-      <button onClick={increasePoints}>+{incrementor}</button>
-      <br />
-      <button onClick={updateIncrementor}>
-        Pay 10 points to change from +{incrementor} to +{incrementor + 1}
-      </button>
+      <div className={visibility === `hidden` ? `notHidden` : `hidden`}>
+        <h1>Current Score: {points}</h1>
+        <button onClick={increasePoints}>+{incrementor}</button>
+        <br />
+        <button onClick={updateIncrementor}>
+          Pay 10 points to change from +{incrementor} to +{incrementor + 1}
+        </button>
+      </div>
+      <div className={visibility}>
+        <h2>You Win!</h2>
+        <button onClick={resetGame}>Play again?</button>
+      </div>
     </main>
   );
 }
