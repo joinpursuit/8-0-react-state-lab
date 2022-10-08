b70ef890cd2398530bf2926a4711f806a0d97df7
NRiddick113 <NatashaRiddick@pursuit.org>
2022-10-08 19:46:54 -0400

    State lab

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
-}
+  const [score,setScore]= useState(0)
+  const [counter,setCounter]= useState(1)
 
-export default App;
+  function Score() {
+    setScore(score + counter) 
+  }
+
+  function increaseCounter() {
+    if (score >= 10) {
+      setScore(score - 10)
+      setCounter(counter + 1)
+    }else{
+      alert(`You can't afford that!`)
+    }
+    
+  }
+  
+  function resetGame() {
+    setScore(0)
+    setCounter(1)
+  }
+  
+
+    if(score < 100 ){
+      return (
+        <main>
+        <h1>Current Score: {score} </h1> 
+        <div>
+        <button id="add" onClick={Score} > +{counter}</button>
+        </div>
+        <div>
+        <button onClick={increaseCounter}>Pay 10 points to change from +{counter} to +{counter + 1}
+        </button>
+        </div>
+
+        </main>
+      )
+    }else{
+      return(
+        <main>
+          <h2>You Win!</h2>
+          <button onClick={resetGame}>Play Again?</button>
+        </main>
+      )
+    }
+}
+      export default App;
