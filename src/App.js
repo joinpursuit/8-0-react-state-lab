6dfc001e8f3a361a64b852957a6f790e82d387e1
chrisking718 <chrisking@pursuit.org>
2022-10-07 23:23:39 -0400

    5 of 9 test passed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+// import { useState } from "react";
+
+
 
 function App () {
+
+  const [userNum,setNum] = useState(0)
+  const [x,moreX] = useState(1)
+  const [y,moreY] = useState(2)
+
+const addX = () =>{
+  console.log(userNum)
+   if(userNum <=100){
+    setNum( userNum + x)
+  }
+  
+}
+
+const increaseX = () =>{
+  if(userNum>=10){
+    moreX(x+1)
+    moreY(y+1)
+    setNum(userNum-10)
+  }else{ alert("You can't afford that!")}
+
+  
+}
     return (
       <main>
         <p>React State Lab</p>
+        <button onClick={addX}>+{x}</button>
+        <h3>Current Score: {userNum}</h3>
+        <button onClick={increaseX}>Pay 10 points to change from +{x} to +{y}</button>
       </main>
     );
 }
