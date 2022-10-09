3465ac5783a930066f10b3b59c5b1f7e6ae5cd52
MarsiaRochelin <marsrochelin@pursuit.org>
2022-10-09 19:47:48 -0400

    All Test Passing

--- src/App.js
+++ src/App.js
@@ -9,27 +9,42 @@ function App () {
 
   const updateScore = () => {
    setScore(score + x)
+   
     }
 
   const fromXToY = () => {
-
-    if(score < 10){
-      setScore(alert(`You can't afford that!`))
+     if(score >= 10){
+      setScore(score - 10)
+      setX(x + 1)
+      setY(y + 1)
+    } else {
+      alert(`You can't afford that!`)
+    }
+    
+    }
+    const playAgain = () => {
+      setScore(0)
+      setX(1)
+      setY(2)
     }
 
-     if(score <= 10){
-      setScore(score - x) 
-    } 
-    }
-
+    if(score < 100){
       return (
         <main>
-          <p>Current Score: {score}</p>
+          <h1>Current Score: {score}</h1>
           <button onClick={updateScore}>+{x}</button>
-          <button onClick={fromXToY}>Pay 10 points to change from ={x} to +{y}</button>
+          <button onClick={fromXToY}>Pay 10 points to change from +{x} to +{y}</button>
         </main>
-      );
+      )
+     } else {
+      return (
+        <main>
+        <h2>You Win!</h2>
+      <button onClick={playAgain}>Play again?</button>
+        </main>
+      )
+    }
+      
   }
-
 
 export default App;
