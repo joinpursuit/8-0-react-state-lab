0647ad3ab022cb07f16b2f46e3ce992dbdc3401f
chrisking718 <chrisking@pursuit.org>
2022-10-09 16:16:46 -0400

    all tests passed

--- src/App.js
+++ src/App.js
@@ -6,28 +6,33 @@ import "./App.css";
 
 function App () {
 
-  const [userNum,setNum] = useState(0)
-  const [x,moreX] = useState(1)
-  const [y,moreY] = useState(2)
+  let [userNum,setNum] = useState(0)
+  let [x,moreX] = useState(1)
+  let [y,moreY] = useState(2)
 
 const addX = () =>{
   console.log(userNum)
    if(userNum <=100){
     setNum( userNum + x)
   }
+    
   
 }
+
 
 const increaseX = () =>{
   if(userNum>=10){
     moreX(x+1)
     moreY(y+1)
     setNum(userNum-10)
-  }else{ alert("You can't afford that!")}
+  }else{ alert("You can't afford that!")
+    }
 
+    
   
 }
-    return (
+ if(userNum <100) { 
+  return (
       <main>
         <p>React State Lab</p>
         <button onClick={addX}>+{x}</button>
@@ -35,6 +40,28 @@ const increaseX = () =>{
         <button onClick={increaseX}>Pay 10 points to change from +{x} to +{y}</button>
       </main>
     );
+ }else{
+
+  const newGame = () =>{
+  
+    console.log("clicked")
+    setNum(userNum = 0);
+      moreX(x = 1)
+      moreY(y=2)
+      
+    
+  }
+  
+  
+
+  return (
+    <main>
+      <h2>You Win!</h2>
+      <h3>Score: {userNum}</h3>
+      <button onClick={newGame}>Play again?</button>
+    </main>
+  
+ )}
 }
 
 export default App;
