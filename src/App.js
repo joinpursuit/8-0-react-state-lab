e692f09f0b72c48d5a62b61d622032b7955400c2
MarsiaRochelin <marsrochelin@pursuit.org>
2022-10-09 16:16:18 -0400

    passing 3 test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from "react";
 import "./App.css";
+import {useState} from 'react'
 
 function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
-}
+  const [score, setScore] = useState(0)
+  const [x, setX] = useState(1)
+  const [y, setY] =useState(2)
+
+  const updateScore = () => {
+   setScore(score + x)
+    }
+
+  const fromXToY = () => {
+
+    if(score < 10){
+      setScore(alert(`You can't afford that!`))
+    }
+
+     if(score <= 10){
+      setScore(score - x) 
+    } 
+    }
+
+      return (
+        <main>
+          <p>Current Score: {score}</p>
+          <button onClick={updateScore}>+{x}</button>
+          <button onClick={fromXToY}>Pay 10 points to change from ={x} to +{y}</button>
+        </main>
+      );
+  }
+
 
 export default App;
