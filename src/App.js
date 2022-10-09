88c20068690d4675159ca973970fa18497067297
Kylie-Lang1 <kylielang@pursuit.org>
2022-10-08 21:06:56 -0400

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+  const [score, setScore] = useState(0);
+  const [points, setPoints] = useState(1);
+
+  const counter = (score) => {
+    setScore(score + points);
+  }
+
+  const increasePoints = (points) => {
+    setScore(score - 10);
+    setPoints(points++);
+  }
+      
+  return (
+    <main>
+      <h1>Current Score: {score}</h1>
+      <button onClick={counter}>+{points}</button>
+      <button onClick={increasePoints}>Pay 10 points to change from +{points} to +{points+1}</button>
+    </main>
+  );
 }
 
 export default App;
