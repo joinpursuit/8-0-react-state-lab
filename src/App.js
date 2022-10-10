d67bf41db5ecbaca09ede6d73c5f18561520e556
TrianePeart <TrianePeart@gmail.com>
2022-10-09 20:41:17 -0400

    Save Point 8 test Passing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { useState } from 'react'
 function App () {
   const [currentScore, setScore] = useState(0)
   const [incrementor, setIncrementor] = useState(1)
+  const [visible, setVisible] = useState(false)
 
   const handleIncreaseIncrementor = () => {
     if(currentScore > 9){
@@ -23,17 +24,18 @@ function App () {
   const resetGame = () => {
     setScore(0)
     setIncrementor(1)
+    setVisible(false)
   }
 
   
     return (
       <main>
         <h2>Current Score: {currentScore}</h2>
-        <button style={currentScore >= 100 ? {display: "none"} : null} onClick={handleIncreaseScore}>+{incrementor}</button>
+        <button style={currentScore >= 100 ? {visible} : null} onClick={handleIncreaseScore}>+{incrementor}</button>
         <br /><br />
-        <button style={currentScore >= 100 ? {display: "none"} : null} onClick={handleIncreaseIncrementor}>Pay 10 points to change from +{incrementor} to +{incrementor + 1}</button>
-        <h2 style={currentScore <= 99 ? {display: "none"} : null}>You Win!</h2>
-        <button style={currentScore <= 99 ? {display: "none"} : null} onClick={resetGame}>Play Again?</button>
+        <button style={currentScore >= 100 ? {visible} : null} onClick={handleIncreaseIncrementor}>Pay 10 points to change from +{incrementor} to +{incrementor + 1}</button>
+        <h2 style={currentScore <= 99 ? {visible} : null}>You Win!</h2>
+        <button style={currentScore <= 99 ? {visible} : null} onClick={resetGame} type = "submit">Play again?</button>
       </main>
     );
 }
