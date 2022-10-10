39e548e4167823dbda6622206b441136a128a42f
Gillesedgy <edgygilles@pursuit.org>
2022-10-10 16:06:03 -0400

    added a bonus function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React from "react";
 import "./App.css";
 import { useState } from "react";
-// import End from "./Components/End";
+//!
+const colors = [
+  "burlywood",
+  "coral",
+  "red",
+  "lightgoldenrodyellow",
+  "purple",
+  "brown",
+  "cadetblue",
+  "cartreuse",
+  "floralwhite",
+];
 
 function App() {
+  //! ------------ Bonus Section ----------
+  //* Backfround Color initial state set to, white
+  const [color, setColor] = useState("peachpuff");
+  // * random color
+  const changeColor = () => {
+    const colorIndex = Math.floor(Math.random() * colors.length);
+    console.log(colorIndex);
+    //! to get random color
+    setColor(colors[colorIndex]);
+  };
+  //! ------------  --------------
+
   //* State initialized to 0.
   const [counter, setCounter] = useState(0);
   //* Initialized incrementor to 1
   let [count, setCount] = useState(+1);
   //* Decrementor ->10 for upgrading points / Alert message
-  const decrementor = () => {
+  const decrementer = () => {
     //* Incrementer for counter
-    // setCounter(counter += 1);
     if (counter < 10) {
       alert("You can't afford that!");
     } else {
@@ -28,13 +50,12 @@ function App() {
     setCounter(0);
     setCount(1);
   };
-  // const play document.querySelector
   //* End Game Victory Screen
   if (counter >= 100) {
     return (
       <main>
-        <div>
-          <h2 className={""}>You Win!</h2>
+        <div style={{ height: "100vh", backgroundColor: "forestgreen" }}>
+          <h2>You Win!</h2>
           <button className="PlayAgain" onClick={PlayAgain}>
             Play again?
           </button>
@@ -43,14 +64,19 @@ function App() {
     );
   } else {
     return (
-      <main>
+      <main style={{ height: "100vh", backgroundColor: color }}>
         <h1>Current Score: {counter}</h1>
         <div className="Btn1">
           <button className="Incrementor" onClick={IncreaseScore}>
             +{count}
           </button>
           <br />
-          <button onClick={decrementor}>
+          <button
+            onClick={() => {
+              decrementer();
+              changeColor();
+            }}
+          >
             Pay 10 points to change from +{count} to +{count + 1}
           </button>
         </div>
--- src/Components/End.js
+++ src/Components/End.js
@@ -2,18 +2,18 @@
 
 // };
 
-const End = () => {
-  return (
-    <main>
-      {/* <h2>You win!</h2> */}
-      <div>
-        <h2 className={""}>You Win!</h2>
-        <button className="PlayAgain" onClick={window.location.reload()}>
-          Play again?
-        </button>
-      </div>
-    </main>
-  );
-};
+// const End = () => {
+//   //   return (
+//   //     <main>
+//   //       {/* <h2>You win!</h2> */}
+//   //       <div>
+//   //         <h2 className={""}>You Win!</h2>
+//   //         <button className="PlayAgain" onClick={window.location.reload()}>
+//   //           Play again?
+//   //         </button>
+//   //       </div>
+//   //     </main>
+//   //   );
+// };
 
-export default End;
+// export default End;
