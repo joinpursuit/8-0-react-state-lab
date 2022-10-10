0fb2ecce260f03a66ba670f193b2ae82a3079724
DestinyJoyner <destinyjoyner@pursuit.org>
2022-10-09 21:06:55 -0400

    all test passed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { useState } from "react";
 import DefaultGame from "./Components/DefaultGame";
-import Victory from "./Components/Victory";
+
 
 function App () {
 
--- src/Components/DefaultGame.js
+++ src/Components/DefaultGame.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { useState } from "react";
-import Victory from "./Victory";
 
 
-function DefaultGame({scoreStart}) {
+function DefaultGame() {
   const [score, setScore] = useState(0)
   const [plusOneButton, setPlusOneButton] = useState(1)
  
+  //Function for newGame
+  function newGame() {
+    setScore(0)
+    setPlusOneButton(1)
+    return(
+      <DefaultGame />
+    )
+  }
+
 // Function for payTenPointsButton
 function payTenPoints() {
   if (score - 10 < 0){
@@ -22,36 +30,28 @@ function payTenPoints() {
 function addButtonValue() {
   const newScore = score + plusOneButton
   setScore(newScore)
-}  
+}
+
+// Game win conditional 
 if(score >= 100){
   return(
-    <Victory />
+    <>
+      <h1>You Win!</h1>
+      <h2>Score: {score}</h2>
+      <button onClick={()=>{newGame()}}>Play again?</button>
+    </>
   )
 }
-else {
+
   return (
     <>
       <h1>Current Score: {score}</h1>
-      <button
-        className="plusOne"
-        onClick={() => {
-          addButtonValue();
-        }}
-      >
-        +{plusOneButton}
-      </button>
-      <button
-        className="tenPoints"
-        onClick={() => {
-          payTenPoints();
-        }}
-      >
-        Pay 10 points to change from +{plusOneButton} to +{plusOneButton + +1}
-      </button>
+      
+      <button onClick={() => {addButtonValue()}}>+{plusOneButton}</button>
+
+      <button onClick={() => {payTenPoints()}}>Pay 10 points to change from +{plusOneButton} to +{plusOneButton + +1}</button>
     </>
   );
-}
-  
 }
 
 export default DefaultGame;
