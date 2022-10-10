a26e7b376c8cc62b2bb6c69fc5053b7e14b4a19d
Kylie-Lang1 <kylielang@pursuit.org>
2022-10-10 19:24:59 -0400

    Game functions. Missing few tests

--- src/App.js
+++ src/App.js
@@ -6,22 +6,44 @@ function App () {
   const [score, setScore] = useState(0);
   const [points, setPoints] = useState(1);
 
-  const counter = (score) => {
+  const counter = () => {
     setScore(score + points);
   }
 
-  const increasePoints = (points) => {
-    setScore(score - 10);
-    setPoints(points++);
+  const increasePoints = () => {
+    if (score >= 10) {
+      setScore(score - 10)
+      setPoints(points + 1)
+    } else {
+      window.alert("You can't afford that!")
+    }
+  }
+
+  const reset = () => {
+    setScore(0)
+    setPoints(1)
+  }
+
+  if (score < 100) {
+    return (
+      <main>
+        <h1>Current Score: {score}</h1>
+        <button onClick={counter}>+{points}</button>
+        <br></br>
+        <button onClick={increasePoints}>Pay 10 points to change from +{points} to +{points+1}</button>
+      </main>
+    )
+  } else {
+    return (
+      <main>
+        <h1>Current Score: {score}</h1>
+        <h2>You Win!</h2>
+        <button onClick={reset}>Play again?</button>
+      </main>
+    )
   }
       
-  return (
-    <main>
-      <h1>Current Score: {score}</h1>
-      <button onClick={counter}>+{points}</button>
-      <button onClick={increasePoints}>Pay 10 points to change from +{points} to +{points+1}</button>
-    </main>
-  );
+
 }
 
 export default App;
