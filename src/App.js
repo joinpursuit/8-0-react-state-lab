ce10ee41a46ee6dcccd8f80ad06c60ec786e39a0
NikeshW <nikeshwankhade@pursuit.org>
2022-10-11 00:54:59 -0400

    completed-react-state-lab

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import React from "react";
 import "./App.css";
+import GameOn from "./Components/GameOn";
+import Winner from "./Components/Winner";
 
-function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+function App() {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [incrementor, setIncrementor] = useState(1);
+
+  return (
+    <main>
+      {currentScore < 100 ? (
+        <>
+          <GameOn
+            currentScore={currentScore}
+            setCurrentScore={setCurrentScore}
+            incrementor={incrementor}
+            setIncrementor={setIncrementor}
+          />
+        </>
+      ) : (
+        <>
+          <Winner
+            setCurrentScore={setCurrentScore}
+            setIncrementor={setIncrementor}
+          />
+        </>
+      )}
+    </main>
+  );
 }
 
 export default App;
