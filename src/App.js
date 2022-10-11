0d4460d1fa2b32834bff4ca811c49dad4911e655
bowersjuan <bowersjuan@gmail.com>
2022-10-11 18:02:44 -0400

    update clicker game app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
-  }
+function App() {
+  const [score, setScore] = useState(0);
+  const [rate, setRate] = useState(1);
+
+  const increaseScore = () => {
+    setScore(score + rate);
+  };
+
+  const increaseRate = () => {
+    if (score >= 10) {
+      setRate(rate + 1);
+      setScore(score - 10);
+    } else alert("You can't afford that!");
+  };
+
+  const resetGame = () => {
+    setScore(0);
+    setRate(1);
+  };
+
+  return (
+    <main>
+      <h1>React State Lab</h1>
+      <div className="scoreAndWinCard">
+        {score < 100 ? <p>Current Score: {score}</p> : null}
+        {score >= 100 ? <h2>You Win!</h2> : null}
+      </div>
+      <div className="increaseScoreButton">
+        {score < 100 ? <button onClick={increaseScore}>+{rate}</button> : null}
+      </div>
+      <div className="increaseRateButton">
+        {score < 100 ? (
+          <button onClick={increaseRate}>
+            Pay 10 points to change from +{rate} to +{rate + 1}
+          </button>
+        ) : null}
+      </div>
+      <div className="resetGameButton">
+        {score >= 100 ? <button onClick={resetGame}>Play again?</button> : null}
+      </div>
+    </main>
+  );
 }
 
 export default App;
