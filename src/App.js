2d3ed73d3091cee253f97d25092b3c46125c9623
Karyn Huston <huston.karyn@gmail.com>
2022-10-12 02:05:03 -0400

    all tests are passing🔥

--- src/App.js
+++ src/App.js
@@ -5,42 +5,52 @@ import "./App.css";
  * 
  *  const [counter, setCounter] = useState(0) 
 console.log(counter)
-
-//counter===value
-//function to set or update value
+counter===value
+setCounter ===to set or update value
  */
 
 function App() {
   let [number, increaseNumber] = useState(0);
 
-  let [buttonValue, increaseButton] = useState(+1);
+  let [buttonValue, increaseButton] = useState(1);
 
-  function increase() {
-    increaseNumber((number += 1)); //when button is clicked number will increase by 1
+  let increase = () => {
+    increaseNumber((number += buttonValue)); //when button is clicked number will increase by 1
 
-    if (number === 10) {
-      increaseButton((buttonValue += 1));
+    //if (number === 10) increaseButton((buttonValue += 1));
+  };
+
+  let payUp = () => {
+    if (number < 10) {
+      alert("You can't afford that!");
+    } else {
+      increaseNumber(number - 10);
+      increaseButton(buttonValue + 1);
+      //increaseNumber(number += buttonValue) number value equal to button value
     }
-   
-  }
+  };
 
-  function payUp () {
-
-    if(number <= 10) {
-      alert("no")
-    }else {
-  increaseNumber(number - 10)
-    }
-  }
+  let resetGame = () => {
+    increaseNumber(0);
+    increaseButton(1);
+  };
 
   return (
     <main>
-      <h1>Current Score:{number}</h1>
-      <button onClick={increase}> +{buttonValue} </button>
-      <button onClick={payUp}>
-        Pay 10 points to change from +{buttonValue} to +{(buttonValue += 1)}
-      </button>
-      <button></button>
+      {number >= 100 ? (
+        <>
+          <h1>You Win!</h1>
+          <button onClick={resetGame}>Play again?</button>
+        </>
+      ) : (
+        <>
+          <h1>Current Score: {number}</h1>
+          <button onClick={increase}> +{buttonValue} </button>
+          <button onClick={payUp}>
+            Pay 10 points to change from +{buttonValue} to +{buttonValue + 1}
+          </button>
+        </>
+      )}
     </main>
   );
 }
