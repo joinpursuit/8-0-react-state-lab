83ce7d2d83cf86e3a741967d5c1b0515f5203455
Mendoza Holger <mendozadavidh111@gmail.com>
2022-10-11 22:09:35 -0400

    lab done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
-function App () {
+const App = () => {
+  let [count, setCount] = useState(0); // setting count default to 0
+  let [incrementing, setIncrement] = useState(1); // setting increment to default to 0
+
+  let increment = () => { // increment function adding to count when button +1 is pushed
+    setCount((count += incrementing));
+  };
+
+  let updatedCount = () => {
+    if (count < 10) { // makes sure the user has enough to upgrade
+      alert("You can't afford that!");
+    } else {
+      setCount(count - 10); // if so then increase incrementing and cut 10 from count
+      setIncrement(incrementing+1);
+    }
+  };
+
+  const gameRestart = () => { // restart game when user wins
+    setCount(0);
+    setIncrement(1);
+  };
+
+  if (count >= 100) { // winning message and adds a button to restart
     return (
-      <main>
-        <p>React State Lab</p>
-      </main>
+      <div>
+        <h2>You Win!</h2>
+        <button onClick={gameRestart}>Play again?</button>
+      </div>
     );
-}
+  }
+
+  return ( // shows current data of users game
+    <div>
+      <div>
+        <h2>Current Score: {count}</h2>
+      </div>
+      <div>
+        <button onClick={increment}>+{incrementing}</button>
+      </div>
+      <div>
+        <button onClick={updatedCount}>
+          Pay 10 points to change from +{incrementing} to +{incrementing+1}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
