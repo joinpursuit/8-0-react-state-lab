9118174ddea16b7f2a423435a671f0eb900e4b3b
SasukeDaTony <tonytaylor@pursuit.org>
2022-10-19 17:17:49 -0400

    buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
-function App () {
-    return (
+function App() {
+  const [total, setTotal] = useState(0);
+  const [increase, setIncrease] = useState(1);
+
+  function addToTotal() {
+    setTotal(increase + total);
+  }
+
+  function addToIncrease() {
+    if (total >= 10) {
+      setIncrease(increase + 1);
+      setTotal(total - 10);
+    } else {
+      alert("You can't afford that!");
+    }
+  }
+
+  return (
+    <>
       <main>
-        <p>React State Lab</p>
+        <h4>Current Score: {total} </h4>
+        <button onClick={addToTotal}>+ {increase}</button>
+        <button onClick={addToIncrease}>Pay 10 for +1 to {increase}</button>
       </main>
-    );
+    </>
+  );
 }
 
 export default App;
