0beee430f8980d608f0a455f4c16834963eeea60
SasukeDaTony <tonytaylor@pursuit.org>
2022-10-19 19:13:54 -0400

    final

--- src/App.js
+++ src/App.js
@@ -3,30 +3,45 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [total, setTotal] = useState(0);
+  const [score, setScore] = useState(0);
   const [increase, setIncrease] = useState(1);
 
   function addToTotal() {
-    setTotal(increase + total);
+    setScore(increase + score);
   }
 
   function addToIncrease() {
-    if (total >= 10) {
+    if (score >= 10) {
       setIncrease(increase + 1);
-      setTotal(total - 10);
+      setScore(score - 10);
     } else {
       alert("You can't afford that!");
     }
   }
 
+  function gameReset() {
+    setIncrease(1);
+    setScore(0);
+  }
+
   return (
-    <>
-      <main>
-        <h4>Current Score: {total} </h4>
-        <button onClick={addToTotal}>+ {increase}</button>
-        <button onClick={addToIncrease}>Pay 10 for +1 to {increase}</button>
-      </main>
-    </>
+    <main>
+      {score >= 100 ? (
+        <>
+          <h3>{score}</h3>
+          <h2>You Win!</h2>
+          <button onClick={gameReset}>Play again?</button>
+        </>
+      ) : (
+        <>
+          <h3>Current Score: {score}</h3>
+          <button onClick={addToTotal}>+{increase}</button>
+          <button onClick={addToIncrease}>
+            Pay 10 points to change from +{increase} to +{increase + 1}
+          </button>
+        </>
+      )}
+    </main>
   );
 }
 
