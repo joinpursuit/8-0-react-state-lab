f6863b56fbf67153783d966ae520ef349f9e6d5f
youssefmohamedfarouk <youssefmohamedfarouk@pursuit.org>
2022-10-18 20:31:23 -0400

    Edit Score.jsx to remove Current Score text if win condition met. Passes all cypress tests.

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,7 +1,9 @@
 export default function Score({score, incrementScore}) {
   return (
     <section className="score">
-      <h1>Current Score: {score}</h1>
+        {
+            score >= 100 ? score : <h1>Current Score: {score}</h1>
+        } 
     </section>
   );
 }
