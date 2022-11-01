8cb5ab6b13437ab66b0e964e24c22a0dcee0c4b5
ZayLpursuit <Zaylors@pursuit.org>
2022-11-01 00:40:32 -0400

    Passing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App () {
+const [score,setScore]=useState(0)
+const [increment,setIncrement]=useState(1)
+const [win,setWin]=useState("")
+const [display,setDisplay]=useState("")
+if (score>100){
+  setScore(score)
+  
+  
+}
+if (score<0){
+  setScore(0)
+}
+function updateIncrements(){
+  if (score<10){
+    alert("You can't afford that!")
+  }
+  if(score>=10){
+    setIncrement(increment+1) 
+    setScore(score-10) 
+}
+}
+
+
+
+
     return (
       <main>
-        <p>React State Lab</p>
+        {
+        score>=100? 
+        <>
+       
+        <h2>You Win!</h2>
+        <button onClick={()=>{setScore(0); setIncrement(1)}}>Play again?</button>
+        </>
+        :
+        <>
+        <h1>Current Score: {score}</h1>
+       
+        <button onClick={()=>setScore(score+ increment)}>+{increment}</button>
+        <button onClick={updateIncrements} >Pay 10 points to change from +{increment} to +{increment+1}</button>
+        </>
+        
+        }
+        {/* <h1>Current Score: {score}</h1>
+       
+        <button onClick={()=>setScore(score+ increment)}>+{increment}</button>
+        <button onClick={updateIncrements} >Pay 10 points to change from +{increment} to +{increment+1}</button>
+        <div>{display}</div> */}
       </main>
     );
 }
