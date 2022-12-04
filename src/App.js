d2927afdada454eb5cd9df8368d3145f9935efa8
dilrubamajumder <dilrubamajumder@pursuit.org>
2022-12-04 16:55:06 -0500

    completed and passing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
+import ClickNum from "./components/ClickNum";
+import Winner from "./components/Winner";
 
-function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+function App() {
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+
+  return (
+    <main>
+      {count < 100 ? (
+        <ClickNum
+          count={count}
+          setCount={setCount}
+          multiplier={multiplier}
+          setMultiplier={setMultiplier}
+        />
+      ) : (
+        <Winner 
+        setCount={setCount} 
+        setMultiplier={setMultiplier} 
+        />
+      )}
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/NumberMsgBtm.js
@@ -0,0 +1,21 @@
+// import React from 'react'
+
+// function NumberMsgBtm({count, setCount, multiplier,setMultiplier}) {
+
+//   // function payPoints(){
+//   //   if(count < 10){
+//   // alert ("You can't afford that!")
+//   //   }else{
+//   // setCount(count - 10)  
+//   // setMultiplier(multiplier +1 )
+//   //   }
+//   // }
+
+//   return (
+//     <div>
+//         <button onClick={payPoints}>Pay 10 points to change from {multiplier} to {multiplier +1}</button>
+//     </div>
+//   )
+// }
+
+// export default NumberMsgBtm
